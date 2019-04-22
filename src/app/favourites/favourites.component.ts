efa29e04378cb8aacdb901d02ff1e53ba2133080
Bole <jelenabole@gmail.com>
2019-04-22 02:43:31 +0200

    fixed static service inj

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BeerService } from '../beer-service/beer.service';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.scss'],
+  providers: [BeerService]
 })
 export class FavouritesComponent implements OnInit {
 
